94a970018501443de81e8bc2988422c91ecfd7dc
Timothy <exzos_graalonline@hotmail.com>
2022-06-10 17:10:33 -0400

    Hide connection credentials

--- src/db-connection.js
+++ src/db-connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb+srv://admin:HOtbadyi8PSQFX65@cluster0.xmfb7.mongodb.net/test?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://admin:<password>@cluster0.xmfb7.mongodb.net/<collection>?retryWrites=true&w=majority';
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
